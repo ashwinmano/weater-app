76f0e26c74a92410086c1f93d441b6aca645b9ea
Ashwinmano <ashwin.m@sceince.christuniversity.in>
2020-07-10 02:49:57 +0530

    API key added to the project

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import 'weather-icons/css/weather-icons.css';
 import './App.css';
 import Weather from './components/weather_component';
-function App() {
-  return (
-    <div className="App">
+
+//api call api.openweathermap.org/data/2.5/weather?q=London,uk
+const API_key=" b50943acbba3788abe752897b4cff05f"
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+    }
+    this.getWeather();
+  }
+
+  getWeather=async()=>{
+    const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`)
+    const response =await api_call.json();
+
+    console.log(response);
+  }
+  render(){
+    return(
+      <div className="App">
       <Weather />
     </div>
-  );
+    );
+  }
 }
+
 
 export default App;
